3d119185676740768a258a077c556ebe14ce7d8d
Abid Hassan <abidhassan2073@gmail.com>
2026-08-06 22:29:08 +0530

    add backend url in frontend

--- frontend/app/config/clientServer.js
+++ frontend/app/config/clientServer.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clientServer = axios.create({
-    baseURL: "https://campusforum.onrender.com",
+    // baseURL: "https://campusforum.onrender.com",
+    baseURL: "https://campusforum-backend.vercel.app/",
     withCredentials: true,
 });
 
