dc10ee40a8150c127d5f0185a28732332df1f637
Abid Hassan <abidhassan2073@gmail.com>
2026-08-15 14:42:41 +0530

    removing if condition statement from profil server comp

--- frontend/app/(main)/profile/page.jsx
+++ frontend/app/(main)/profile/page.jsx
@@ -2,15 +2,10 @@ import { getProfile } from "@/lib/api";
 import dynamic from "next/dynamic";
 import { cookies } from "next/headers";
 const ProfileUi = dynamic(() => import("@/ui/Profile.jsx"));
-import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
-
-  if (!token) {
-    redirect("/login");
-  }
 
   const { profile, userPosts } = await getProfile(token);
 
